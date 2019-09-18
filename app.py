a412194ff0aec498613e5e9b1e7853b12e746386
Jessica Smith <30677679+jSith@users.noreply.github.com>
2019-09-18 10:57:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ def hello_world():
     return 'Hello, World!'
     
 if __name__ == "__main__": 
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
