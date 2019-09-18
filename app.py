75f7d63abb85fc5a2c32826545bc8aa054f5424a
Jessica Smith <30677679+jSith@users.noreply.github.com>
2019-09-18 11:16:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import os
+from random import choice
 
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
+BOT_ID = '9798531b12d783b20b9b5334ab'
+GROUPME = 'https://api.groupme.com/v3/bots/'
 
 @app.route('/api/')
 def hello_world():
     return 'Hello, World!'
+
+def _get_message(): 
+    possible_messages = ['Okay', 'No', 'Yes', 'A debilitating surgery would', 'Thanks']
+    message_base = choice(possible_messages)
+    message = message_base + ' - RyBot'
+    return message
+
+@app.route('/api/rybot', methods='POST')
+def bot(): 
+    input = request.json
+    message = _get_message()
+    body = {'bot_id': BOT_ID, 'text': message}
+    requests.post(GROUPME + 'post', data=body)
     
 if __name__ == "__main__": 
     port = int(os.environ.get('PORT', 5000))
