efffcb19cd9c332ff4013f9f51dd1b09ed14b1a8
Jessica Smith <30677679+jSith@users.noreply.github.com>
2021-03-08 19:24:27 -0600

    add error printing

--- app.py
+++ app.py
@@ -322,6 +322,7 @@ def pastabot():
         body = {"bot_id": bot_id, "text": string}
         resp = requests.post(f'{GROUPME}/bots/post', data=body)
         if not resp.ok:
+            print(resp.reason)
             raise ValueError(resp)
 
     if img_url:
